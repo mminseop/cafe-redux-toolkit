1898229cd173649ce0e4bec0661f569734cd1590
mminseop <alstjq903@naver.com>
2025-07-16 13:17:25 +0900

    [Redux Toolkit] React 15일차 과제 완료

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,11 @@
+import { useDispatch, useSelector } from "react-redux";
 import data from "../assets/data";
+import { cartSlice } from "../redux/redux";
 
-function Cart({ menu, cart, setCart }) {
+function Cart() {
+  const menu = useSelector((state) => state.menu);
+  const cart = useSelector((state) => state.cart);
+  console.log(cart);
   if (!menu)
     return (
       <div style={{ textAlign: "center", margin: "80px" }}>
@@ -20,8 +25,6 @@ function Cart({ menu, cart, setCart }) {
               item={allMenus.find((menu) => menu.id === el.id)}
               options={el.options}
               quantity={el.quantity}
-              cart={cart}
-              setCart={setCart}
             />
           ))
         ) : (
@@ -32,7 +35,8 @@ function Cart({ menu, cart, setCart }) {
   );
 }
 
-function CartItem({ item, options, quantity, cart, setCart }) {
+function CartItem({ item, options, quantity }) {
+  const dispatch = useDispatch();
   return (
     <li className="cart-item">
       <div className="cart-item-info">
@@ -41,7 +45,7 @@ function CartItem({ item, options, quantity, cart, setCart }) {
       </div>
       <div className="cart-item-option">
         {Object.keys(options).map((el) => (
-          <div key={el.id}>
+          <div key={el}>
             {el} : {data.options[el][options[el]]}
           </div>
         ))}
@@ -50,7 +54,8 @@ function CartItem({ item, options, quantity, cart, setCart }) {
       <button
         className="cart-item-delete"
         onClick={() => {
-          setCart(cart.filter((el) => item.id !== el.id));
+          // setCart(cart.filter((el) => item.id !== el.id));
+          dispatch(cartSlice.actions.removeFromCart(item.id));
         }}
       >
         삭제
